d107018869777d9c3b388aa53f326dbf8d0a3515
Eric M. Aldrich <ealdrich@gmail.com>
2017-08-30 09:53:00 +0200

    Added new config for 5 groups of 6 players.

--- config/createRedwoodConfigs.py
+++ config/createRedwoodConfigs.py
@@ -7,7 +7,7 @@ periods = int(sys.argv[3])
 exchangeType = "cda"
 subject = "default"
 startingWealth = 140
-speedCost = 0.1
+speedCost = 0.02
 maxSpread = 5
 marketEventsURL = "https://dl.dropboxusercontent.com/s/j0qsfuedcgmuiks/investorData.csv?dl=1"
 priceChangesURL = "https://dl.dropboxusercontent.com/s/4i20xxhqewl9y4d/jumpData.csv?dl=1"

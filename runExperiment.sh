319bbe841c8e075e9b7bacad41852d92282629f1
Eric M. Aldrich <ealdrich@gmail.com>
2017-08-30 14:48:26 +0200

    Launch Frankfurt instance again and had to update URI in GroupManager.

--- runExperiment.sh
+++ runExperiment.sh
@@ -1,3 +1,3 @@
 ssh root@games.max.pm "sudo service redwood-router restart; exit;"
 echo "cd /var/HFT; ./stop_all.sh; exit;" | ssh -t ubuntu@ec2-54-149-235-92.us-west-2.compute.amazonaws.com
-echo "cd /var/HFT; ./stop_all.sh; ./run_cda_groups.sh 5 1" | ssh -t ubuntu@ec2-54-93-112-219.eu-central-1.compute.amazonaws.com
+echo "cd /var/HFT; ./stop_all.sh; ./run_cda_groups.sh 5 1" | ssh -t ubuntu@ec2-52-59-251-204.eu-central-1.compute.amazonaws.com

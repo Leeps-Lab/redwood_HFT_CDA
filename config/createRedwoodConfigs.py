d3b93cc1533c7c536dab1ddad178377b9f23ed88
Eric M. Aldrich <ealdrich@gmail.com>
2017-08-31 07:22:39 +0200

    Added configs for Cologne pilots, 8-31-2017.

--- config/createRedwoodConfigs.py
+++ config/createRedwoodConfigs.py
@@ -6,10 +6,10 @@ nPlayersPerGroup = int(sys.argv[2])
 periods = int(sys.argv[3])
 exchangeType = "cda"
 subject = "default"
-startingWealth = 50
+startingWealth = 20
 speedCost = 0.02
 maxSpread = 5
-exchangeRate = 4
+exchangeRate = 1
 experimentLength = 300000
 marketEventsURL = "https://dl.dropboxusercontent.com/s/j0qsfuedcgmuiks/investorData.csv?dl=1"
 priceChangesURL = "https://dl.dropboxusercontent.com/s/4i20xxhqewl9y4d/jumpData.csv?dl=1"
@@ -20,7 +20,7 @@ for group in range(1,nGroups+1):
     groupList.append(range((group-1)*nPlayersPerGroup+1,group*nPlayersPerGroup+1))
 
 for period in range(1,periods+1):
-    fName = exchangeType+"_config_"+str(nGroups)+"groups_p"+str(period)+".csv"
+    fName = exchangeType+"_config_"+str(experimentLength/1000)+"s_"+str(nGroups)+"groups_"+str(nPlayersPerGroup)+"_players_"+"period"+str(period)+".csv"
     fOut = open(fName,"w")
     fOut.write("period,subject,groups,startingWealth,speedCost,maxSpread,marketEventsURL,priceChangesURL,experimentLength,exchangeRate,exchangeURI\n")
     fOut.write(str(period)+","+subject+",\""+str(groupList)+"\","+str(startingWealth)+","+str(speedCost)+","+str(maxSpread)+","+marketEventsURL+","+priceChangesURL+","+str(experimentLength)+","+str(exchangeRate)+","+exchangeURI)

4dc42ab46e38a2b5a4a1647aa4b726db50748765
Jason Vranek <jvranek@ucsc.edu>
2017-10-09 07:57:52 -0700

    added delay between user clicks to avoid spam

--- start.js
+++ start.js
@@ -21,6 +21,8 @@ RedwoodHighFrequencyTrading.controller("HFTStartController",
          $scope.jumpOffsetY = 0;
          $scope.LaserSound;
          $scope.statename = "Out";
+         $scope.spamDelay = 300;
+         $scope.isAnimating = false;
 
          $scope.s = {
             NO_LINES: 0,
@@ -355,12 +357,15 @@ RedwoodHighFrequencyTrading.controller("HFTStartController",
 
          $("#graph1")
             .mousedown( function(event) {
+               if($scope.isAnimating){
+                  return;
+               }
+               //only allow mousePressed to register after 
                $scope.mousePressed = true;                                       //set the flag so in case we leave the svg element we know it was a press
-               // if ($scope.state != "state_maker") {
-               //       // var msg = new Message("USER", "UMAKER", [rs.user_id, $scope.tradingGraph.getCurOffsetTime()]);
-               //       // $scope.sendToGroupManager(msg);
-               //       $scope.setState("state_maker");
-               // }  
+               
+               setTimeout(function() {
+                    isAnimating = false;
+                }, $scope.spamDelay);
             })
             .mouseleave( function(event) {
                if ($scope.mousePressed) {                                        //only set the spread if svg has been clicked on
